80b0bfaeea16c7d403b995ea5521a4eaece00a06
PabloLuna44 <guzmanlunajuanpablo@gmail.com>
2024-03-05 18:49:17 -0600

    Resolved problems

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\ExpenseController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EarningController;
 use App\Http\Controllers\LoanController;
-use App\Models\Loan;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EarningController;
 use App\Http\Controllers\InvestmentController;
