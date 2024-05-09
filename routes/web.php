1c740c4dd8470841424822c9dad113eb0d10e178
PabloLuna44 <guzmanlunajuanpablo@gmail.com>
2024-05-08 22:26:06 -0600

    Fix redirect with dashboard

--- routes/web.php
+++ routes/web.php
@@ -75,3 +75,14 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard-jetstream', function () {
+        return view('dashboardJetstream');
+    })->name('dashboardJetstream');
+});
