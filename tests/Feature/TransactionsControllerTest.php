c6c6412698e4967086e717f2d928bb1ec2368dfc
PabloLuna44 <guzmanlunajuanpablo@gmail.com>
2024-05-12 22:35:32 -0600

    Resolved problems with TransactionsTest

--- tests/Feature/TransactionsControllerTest.php
+++ tests/Feature/TransactionsControllerTest.php
@@ -14,15 +14,14 @@ class TransactionsControllerTest extends TestCase
     {
         parent::setUp();
 
+        $this->user = User::factory()->create();
 
-        $user = User::factory()->create();
-
-        $this->actingAs($user);
+        $this->actingAs($this->user);
     }
 
     public function test_transaction_index(): void
     {
-        $response = $this->get('/transactions');
+        $response = $this->get(route('transactions.index'));
 
 
         $response->assertStatus(200) //estado 200 generalmente indica que la página solicitada fue encontrada y cargada con éxito.
@@ -34,47 +33,47 @@ class TransactionsControllerTest extends TestCase
     public function test_create_transaction(): void
     {
         // Datos que se enviarán en la solicitud POST
-        $account = Account::factory()->create();
+        $account = Account::factory()->create(['user_id' => $this->user->id]);
         $data = Transaction::factory()->for($account)->create()->toArray();
 
 
-        $response = $this->post('/transactions', $data);
+        $response = $this->post(route('transactions.store', $data));
 
-
+        $this->assertDatabaseHas('transactions', $data);
         $response->assertStatus(302) // Verificar que la respuesta tiene un estatus 302 que es de una redireccion con exito
-            ->assertRedirect('/transactions');
+            ->assertRedirect(route('transactions.index'));
     }
 
     public function test_bad_create_transaction(): void
     {
         // Datos que se enviarán en la solicitud POST
-        $account = Account::factory()->create();
+        $account = Account::factory()->create(['user_id' => $this->user->id]);
         $data = Transaction::factory()->for($account)->make()->toArray();
         // Modificar los datos para que sean inválidos
-        $data['amount'] = -100; // Monto de transacción negativophp 
+        $data['amount'] = -100; // Monto de transacción negativo
 
 
+        $response = $this->post(route('transactions.store', $data));
 
-        $response = $this->post('/transactions', $data);
 
-        
         $response->assertStatus(302)
             ->assertSessionHasErrors('amount');
     }
 
 
-    
+
     public function test_delete_transaction(): void
     {
         // Datos que se enviarán en la solicitud POST
-        $account = Account::factory()->create();
+        $account = Account::factory()->create(['user_id' => $this->user->id]);
         $data = Transaction::factory()->for($account)->create()->toArray();
-        
-        
 
-        $response = $this->delete('/transactions/'. $data['id']);
+        $response = $this->delete(route('transactions.destroy', $data['id']));
+
+
+        $this->assertSoftDeleted('transactions', $data);
 
         $response->assertStatus(302)
-            ->assertRedirect('/transactions'); 
+            ->assertRedirect(route('transactions.index'));
     }
 }
